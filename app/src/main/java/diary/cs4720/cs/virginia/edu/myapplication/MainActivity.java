9e90ab4c1081f2cf682da6a083e58a14344d9ecd
Yugank <Singhal>
2015-09-16 13:22:47 -0400

    basic requirements for milestone 1

--- app/src/main/java/diary/cs4720/cs/virginia/edu/myapplication/MainActivity.java
+++ app/src/main/java/diary/cs4720/cs/virginia/edu/myapplication/MainActivity.java
@@ -1,18 +1,27 @@
 package diary.cs4720.cs.virginia.edu.myapplication;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
-    ArrayList<DiaryEntry> entryList = new ArrayList<DiaryEntry>();
+    public ArrayList<DiaryEntry> entryList = new ArrayList<DiaryEntry>();
     ArrayAdapter<DiaryEntry> adapter;
 
     @Override
@@ -20,11 +29,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0, 0, this);
+        Log.d("MAIN ACTIVITY","past registration");
 
-        ListView listView = (ListView)findViewById(R.id.listView);
+/*        ListView listView = (ListView)findViewById(R.id.listView);
         adapter = new ArrayAdapter<DiaryEntry>(this, android.R.layout.simple_list_item_1, entryList);
 
-        listView.setAdapter(adapter);
+        listView.setAdapter(adapter);*/
     }
 
     @Override
@@ -49,5 +61,36 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void Repeat(View view){
+        EditText editText = (EditText)findViewById(R.id.editText);
+        TextView repeat =  (TextView) findViewById(R.id.Repeat);
+        repeat.setText(editText.getText().toString());
+    }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        TextView Lat = (TextView) findViewById(R.id.Latitude);
+        TextView Long = (TextView) findViewById(R.id.Longitude);
+        Lat.setText("Latitude: " + location.getLatitude());
+        Long.setText("Longitude: " + location.getLongitude());
+        Log.d("LOCATION", "" + location.getLongitude());
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        TextView Lat = (TextView) findViewById(R.id.Latitude);
+        TextView Long = (TextView) findViewById(R.id.Longitude);
+        Lat.setText("OFF");
+        Long.setText("OFF");
+    }
 }
